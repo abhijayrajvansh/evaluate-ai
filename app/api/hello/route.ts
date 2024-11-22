27479369199bc35277e5f7ff5a0a877534d9a13c
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-11-22 12:49:28 +0530

    chore (api): hello & check health api GET

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,13 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   return NextResponse.json({
     status: 200,
-    server: 'running'
-  })
-}
-
-export async function POST(req: NextRequest) {
-  return NextResponse.json({
-    status: 200,
-    server: 'running'
-  })
+    message: "hello, server is running!",
+  });
 }
