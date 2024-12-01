530b8b776bb2ca961fcf7b111eefcfe06c7c49ee
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-01 17:50:34 +0530

    chore: added sample dashboard route and page

---
+++ components/pages/Dashboard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { MoveLeft } from "lucide-react";
+
+const Dashboard = () => {
+  return (
+    <div className="container mx-auto w-full h-screen flex items-center justify-center">
+      <div className="text-center">
+        <p className="font-serif text-5xl">Dashboard</p>
+        <Button asChild variant={"link"} className="mt-7 text-lg">
+          <Link href={"/"}>
+            <MoveLeft />
+            return back
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
