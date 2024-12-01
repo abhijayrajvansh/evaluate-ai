5d43e7d9735141c8bec1bc7c456d482fb58348c0
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-01 17:27:08 +0530

    update: navigation data for products

--- components/parts/NavigationBar.tsx
+++ components/parts/NavigationBar.tsx
@@ -15,41 +15,30 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "../ui/button";
 
-const components: { title: string; href: string; description: string }[] = [
+const products: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
+    title: "AI Resume Parser",
+    href: "/",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "Extract information from resumes and CVs to automate the recruitment process.",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    title: "Generate Objective Tests",
+    href: "/",
     description:
-      "For sighted users to preview content available behind a link.",
+      "Create objective tests for your students or employees in minutes from resume data.",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
+    title: "Github Profile Analyzer",
+    href: "/",
     description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      "Analyze Github profiles to understand the skills and contributions of developers.",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: "Project Analysis & Breakdown",
+    href: "/",
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      "Analyze projects to understand the technologies and tools used.",
   },
 ];
 
@@ -71,41 +60,40 @@ export default function CustomNavigationMenu() {
                       className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                       href="/"
                     >
-                      <div className="h-6 w-6">Logo</div>
+                      <div className="h-6 w-6">evaluate.ai</div>
                       <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
+                        sign up for free!
                       </div>
                       <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components that you can copy and
-                        paste into your apps. Accessible. Customizable. Open
-                        Source.
+                      Analyze resumes, github projects, portfolios and achievements to rank
+                      candidates.
                       </p>
                     </a>
                   </NavigationMenuLink>
                 </li>
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
+                <ListItem href="/docs" title="Documentation">
+                  Learn how to use our AI models and tools.
                 </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
+                <ListItem href="/pricing" title="Credits">
+                  Understand how to configure our tools.
                 </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
+                <ListItem href="/docs/ranking-system" title="Ranking System">
+                  Explore our ranking system.
                 </ListItem>
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuTrigger className="text-base">Components</NavigationMenuTrigger>
+            <NavigationMenuTrigger className="text-base">Our Products</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {components.map((component) => (
+                {products.map((product) => (
                   <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
+                    key={product.title}
+                    title={product.title}
+                    href={product.href}
                   >
-                    {component.description}
+                    {product.description}
                   </ListItem>
                 ))}
               </ul>
