05d44ec06f2c85fea93e59275d4a91ae6d6b89c5
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-01 17:36:05 +0530

    update: doc page comming soon ui

--- components/pages/Docs.tsx
+++ components/pages/Docs.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { MoveLeft } from "lucide-react";
 
 const Docs = () => {
   return (
     <div className="container mx-auto w-full h-screen flex items-center justify-center">
       <div className="text-center">
         <p className="font-serif text-5xl">Documentation</p>
-        <p>comming soon...</p>
-        <Button asChild className="mt-7 text-lg px-7 py-5">
-          <Link href={"/"}>Go Back!</Link>
+        <Button asChild variant={'link'} className="mt-7 text-lg">
+          <Link href={"/"}><MoveLeft />return back</Link>
         </Button>
       </div>
     </div>
