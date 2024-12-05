2d84fb46d519aba4c19ccc33df8b4e5336257de2
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-05 17:51:04 +0530

    refactor: mobile-view warning

--- components/parts/MobileViewWarning.tsx
+++ components/parts/MobileViewWarning.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MobileViewWarning = () => {
   return (
-    <div className="text-center lg:hidden flex items-center justify-center h-screen">This web app is not compatible with mobile view, <br /> kindly open it on desktop!</div>
+    <div className="text-center lg:hidden flex items-center justify-center h-screen">
+      This web app is not compatible with mobile view, <br /> kindly open it on desktop!
+    </div>
   )
 }
 
