1724d0bb11aa44b7bd4ec7afef9e1986484db0ee
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-06 21:30:37 +0530

    chore (api): using clone api on server side

--- app/api/methods/index.ts
+++ app/api/methods/index.ts
@@ -1,9 +1,13 @@
+'use server'
+
 import axios from 'axios';
 
 export async function cloneRepo(repoUrl: string) {
   try {
-    await axios.post('/api/clone-repo', { repoUrl });
-    console.log('\n>repo cloned successfully!');
+    await axios.post('http://localhost:3000/api/clone-repo', { repoUrl });
+
+    console.log('\n\n\n>repo cloned successfully!\n>repo cloned successfully!\n>repo cloned successfully!\n\n\n');
+
   } catch (error) {
     console.error('error during cloning:(\n', error);
   }
