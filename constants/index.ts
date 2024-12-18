cc291bbf0cc0aacf8a254a260a5d962c389fa3df
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 00:46:47 +0530

    chore: add modular constants for env

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 
-const NODE_ENV = process.env.NODE_ENV;
+export const NODE_ENV = process.env.NODE_ENV;
+export const DATABASE_URL = process.env.DATABASE_URL;
 
-console.log(NODE_ENV);
+console.log("NODE_ENV: ", NODE_ENV);
+console.log("DATABASE_URL: ", DATABASE_URL);
