a5c48c1ea12fbdc3cb06ff82de82338977da6f84
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 00:48:31 +0530

    update: env support for database

--- server/db/index.ts
+++ server/db/index.ts
@@ -7,11 +7,15 @@ import "dotenv/config";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
+
+const DATABASE_URL = process.env.DATABASE_URL as string;
+const NODE_ENV = process.env.NODE_ENV as string;
+
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const conn = globalForDb.conn ?? postgres(DATABASE_URL);
+if (NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
