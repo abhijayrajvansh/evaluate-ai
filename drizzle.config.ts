cb5b49863310e0705059bb337ac4c82a075af293
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 01:32:27 +0530

    update: satisfies database url as string

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { type Config } from "drizzle-kit";
-import 'dotenv/config';
+import { DATABASE_URL } from "@/config";
 
 export default {
   schema: "./server/db/schema.ts",
   dialect: "postgresql",
   out: "./server/db/migrations",
-  dbCredentials: { url: env.DATABASE_URL as string },
+  dbCredentials: { url: DATABASE_URL as string },
   verbose: true,
   strict: true,
   tablesFilter: ["evaluate-ai_*"],
 } satisfies Config;
+  
