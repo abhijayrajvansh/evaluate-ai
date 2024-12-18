97242d5149431fc4d90c369d2a0520eb25321424
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 01:40:22 +0530

    refactor: typecast variables as string

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
-export const NODE_ENV = process.env.NODE_ENV;
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const NODE_ENV = process.env.NODE_ENV as string;
+export const DATABASE_URL = process.env.DATABASE_URL as string;
 
 const print_env_vars = false;
 
