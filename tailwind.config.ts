48f148869dc1a03f232f4496b779f147d647c9d3
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 00:49:20 +0530

    refactor: target all tsx files from root

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./**/*.tsx"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
