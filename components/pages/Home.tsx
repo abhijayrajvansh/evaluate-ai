ca20f2d6bd718d05fd6894ec0cf244e6e3bf742b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-12-19 15:54:05 +0530

    update: add navigation link to dashboard

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
-    <section>Home</section>
+    <section>
+      <h1 className='text-2xl mb-5'>Home</h1>
+      <Link href={'/dashboard'} className='text-blue-500 underline underline-offset-4'>goto dashboard</Link>
+    </section>
   )
 }
 
